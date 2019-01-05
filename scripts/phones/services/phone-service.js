18503ae10b492180a0201646b75d2cf2319a6844
Roman Zakrevskyi <romul3003@gmail.com>
2019-01-05 20:38:24 +0200

    finished myPromise

--- scripts/phones/services/phone-service.js
+++ scripts/phones/services/phone-service.js
@@ -15,33 +15,76 @@ const PhoneService = {
 
         let promise = this._sendRequest(`phones/${phoneId}.json`);
 
-        promise.then(callback);
-        promise.then(result => {
-            console.log(result);
-        })
+        promise.then(callback, err => {console.log('cb1', err) });
+        promise.catch(err => {console.log('cb2', err) });
+
+        setTimeout(() => {
+            promise.then(result => {
+                console.log(result);
+            })
+        }, 1000);
+
+
     },
 
     _sendRequest(url) {
-        let promise = {
-            _successCallbacks: [],
-
-            then(successCallback) {
-                this._successCallbacks.push(successCallback);
-            },
-
-            _resolve(data) {
-                this._successCallbacks.forEach(callback => callback(data))
-            },
-        };
-
-        HttpService.sendRequest(url, {
-            successCallback(data) {
-                promise._resolve(data);
-            }
+        let promise = new MyPromise((resolve, reject) => {
+            HttpService.sendRequest(url, {
+                successCallback(data) {
+                    resolve(data);
+                },
+                errorCallback(error) {
+                    reject(error);
+                }
+            });
         });
 
         return promise;
     }
 };
+
+class MyPromise {
+    constructor(behaviorFunction) {
+        this._successCallbacks = [];
+        this._errorCallbacks = [];
+        this._status = 'pending';
+        this._result = null;
+
+        behaviorFunction(this._resolve.bind(this), this._reject.bind(this));
+    };
+
+    then(successCallback, errorCallback) {
+        if (this._status === 'fullfilled') {
+            successCallback(this._result);
+        } else if (this._status === 'rejected') {
+            errorCallback(this._result);
+        } else {
+            this._successCallbacks.push(successCallback);
+            if (errorCallback) {
+                this._errorCallbacks.push(errorCallback);
+            }
+        }
+    };
+
+    catch(errorCallback) {
+        if (this.status === 'rejected') {
+            errorCallback(this._result);
+        } else {
+            this._errorCallbacks.push(errorCallback);
+        }
+    }
+
+    _resolve(data) {
+        this._status = 'fullfilled';
+        this._result = data;
+        this._successCallbacks.forEach(callback => callback(data));
+    };
+
+    _reject(error) {
+        this._status = 'rejected';
+        this._result = error;
+        this._errorCallbacks.forEach(callback => callback(error));
+    }
+}
 
 export default PhoneService;
--- scripts/shared/services/http-service.js
+++ scripts/shared/services/http-service.js
@@ -1,19 +1,25 @@
 const BASE_URL = location.pathname;
 
 const HttpService = {
-    sendRequest(url, { method = 'GET', successCallback }) {
+    sendRequest(url, { method = 'GET', successCallback, errorCallback }) {
         let xhr = new XMLHttpRequest();
         xhr.open(method, BASE_URL + url, true);
         xhr.send();
 
         xhr.onload = () => {
+            if (xhr.status !== 200) {
+                errorCallback(xhr.status + ': ' + xhr.statusText);
+                return;
+            }
+
             let responseData = JSON.parse(xhr.responseText);
             successCallback(responseData);
         };
 
-        xhr.onerror = () => {
-            console.error(xhr.status + ': ' + xhr.statusText);
-        }
+        // xhr.onerror = () => {
+        //     let callback = errorCallback || console.error;
+        //     callback(xhr.status + ': ' + xhr.statusText);
+        // }
     },
 };
 
