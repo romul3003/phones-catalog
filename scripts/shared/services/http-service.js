f70a889f1a354477f8f37f86d1178e118d407473
Roman Zakrevskyi <romul3003@gmail.com>
2019-01-26 13:31:44 +0200

    refactored http-service using fetch

--- scripts/shared/services/http-service.js
+++ scripts/shared/services/http-service.js
@@ -2,21 +2,23 @@ const BASE_URL = location.pathname;
 
 const HttpService = {
     sendRequest(url, method = 'GET') {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open(method, BASE_URL + url, true);
-            xhr.send();
+        // return new Promise((resolve, reject) => {
+        //     let xhr = new XMLHttpRequest();
+        //     xhr.open(method, BASE_URL + url, true);
+        //     xhr.send();
+        //
+        //     xhr.onload = () => {
+        //         if (xhr.status !== 200) {
+        //             reject(xhr.status + ': ' + xhr.statusText);
+        //             return;
+        //         }
+        //
+        //         let responseData = JSON.parse(xhr.responseText);
+        //         resolve(responseData);
+        //     };
+        // });
 
-            xhr.onload = () => {
-                if (xhr.status !== 200) {
-                    reject(xhr.status + ': ' + xhr.statusText);
-                    return;
-                }
-
-                let responseData = JSON.parse(xhr.responseText);
-                resolve(responseData);
-            };
-        });
+        return fetch(BASE_URL + url).then(response => response.json());
     }
 };
 
